950ace6e016d3532aefa3a46d8ca8db7fa764833
Parallax18 <rhythmist204@gmail.com>
2023-02-20 21:10:58 +0100

    fix: ui updates

--- src/components/pricing/PricingCard.tsx
+++ src/components/pricing/PricingCard.tsx
@@ -1,11 +1,22 @@
 import { Box, Flex, HStack, Stack, Text, VStack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import React, { useEffect } from 'react';
+import React from 'react';
 
-const PricingCard = ({ Icon, price }: { Icon: React.FC; price?: any }) => {
-  useEffect(() => {
-    console.log({ price });
-  }, [price]);
+const PricingCard = ({
+  Icon,
+  type,
+  subtext,
+  sending_price,
+  receiving_price,
+  unit,
+}: {
+  Icon: React.FC;
+  type: string;
+  subtext: string[];
+  sending_price: string;
+  receiving_price?: string;
+  unit: string;
+}) => {
   return (
     <Box
       bg={'#fff'}
@@ -24,7 +35,7 @@ const PricingCard = ({ Icon, price }: { Icon: React.FC; price?: any }) => {
             <Icon />
           </Box>
           <Text fontFamily="haffer" color={'brand.black'} fontWeight={'bold'}>
-            SMS
+            {type}
           </Text>
         </Stack>
 
@@ -36,51 +47,61 @@ const PricingCard = ({ Icon, price }: { Icon: React.FC; price?: any }) => {
         >
           <Stack spacing={0}>
             <Text fontSize={'md'} fontWeight={'light'}>
-              To Send SMS
+              {subtext[0]}
             </Text>
             <Text fontWeight={'medium'} fontSize={'lg'}>
-              NGN {price || '40'}
-              <span className="text-base font-normal">/sms</span>
+              {sending_price}
+              <span className="text-base font-normal">/{unit}</span>
             </Text>
           </Stack>
           <VStack spacing={0} alignItems={'end'}>
             <Text fontSize={'md'} fontWeight={'light'}>
-              To Receive SMS
+              {subtext[1]}
             </Text>
-            <Box bg={'brand.pale'} rounded={'full'} w={'max'} px={4} py={1}>
-              <Text
-                fontWeight={'normal'}
-                textAlign={'center'}
-                fontSize={'xs'}
-                color={'brand.primary'}
-              >
-                Coming Soon
+            {receiving_price ? (
+              <Text fontWeight={'medium'} fontSize={'lg'}>
+                {sending_price}
+                <span className="text-base font-normal">/{unit}</span>
               </Text>
-            </Box>
+            ) : (
+              <Box bg={'brand.pale'} rounded={'full'} w={'max'} px={4} py={1}>
+                <Text
+                  fontWeight={'normal'}
+                  textAlign={'center'}
+                  fontSize={'xs'}
+                  color={'brand.primary'}
+                >
+                  Coming Soon
+                </Text>
+              </Box>
+            )}
           </VStack>
         </HStack>
       </Box>
 
-      <Flex
-        borderTop={'1px solid'}
-        borderColor={'brand.outline'}
-        justifyContent={'center'}
-        alignItems={'center'}
-        roundedBottom={'lg'}
-        _hover={{ bg: '#daf3ff' }}
-        p={7}
-        cursor="pointer"
-      >
-        <Text
-          textDecor={'underline'}
-          fontWeight={'normal'}
-          fontFamily={'haffer'}
-          fontSize={'lg'}
-          color={'brand.link'}
-        >
-          More details
-        </Text>
-      </Flex>
+      {type === 'SMS' ||
+        ('Whatsapp' && (
+          <Flex
+            borderTop={'1px solid'}
+            borderColor={'brand.outline'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            roundedBottom={'lg'}
+            _hover={{ bg: '#daf3ff' }}
+            p={7}
+            cursor="pointer"
+          >
+            <Text
+              textDecor={'underline'}
+              fontWeight={'normal'}
+              fontFamily={'haffer'}
+              fontSize={'lg'}
+              color={'brand.link'}
+            >
+              More details
+            </Text>
+          </Flex>
+        ))}
     </Box>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { useRecoilState } from 'recoil';
 
 import {
   MailIcon,
+  SmsIcon,
   VerificationIcon,
   VoiceIcon,
   WhatsAppIcon,
@@ -16,6 +17,47 @@ import { pricingAtom } from '@/recoil/atoms/pricesatom';
 import usePrices from '@/services/hooks/use-prices';
 import { Main } from '@/templates/Main';
 import useMediaWidth from '@/utils/use-media-width';
+
+const services = [
+  {
+    type: 'SMS',
+    subtext: ['To Send SMS', 'To Receive SMS'],
+    sending_price: 'EUR 54.6500',
+    unit: 'sms',
+    icon: SmsIcon,
+  },
+  {
+    type: 'Voice',
+    subtext: ['To make Calls', 'To Receive Calls'],
+    sending_price: 'EUR 165.0000',
+    unit: 'sec',
+    icon: VoiceIcon,
+  },
+  {
+    type: 'Email',
+    subtext: ['Price Per Mail'],
+    sending_price: 'EUR 0.2000',
+    unit: 'email',
+    icon: MailIcon,
+  },
+  {
+    type: 'Whatsapp',
+    subtext: ['To Send Message', 'To Receive Message'],
+    sending_price: 'EUR 2.5750',
+    receiving_price: 'EUR 0.0000',
+    unit: 'msg',
+    icon: WhatsAppIcon,
+  },
+
+  {
+    type: 'Verification',
+    subtext: ['To Send OTP', 'To Confirm OTP'],
+    sending_price: '0.0000',
+    receiving_price: '200000.0000',
+    unit: 'OTP',
+    icon: VerificationIcon,
+  },
+];
 
 const Index = () => {
   const { padding } = useMediaWidth();
@@ -42,10 +84,17 @@ const Index = () => {
       ) : (
         <Box px={padding} transform={'auto'} translateY={[0, '-10%']}>
           <SimpleGrid columns={[1, 3]} spacing={10}>
-            <PricingCard Icon={VoiceIcon} />
-            <PricingCard Icon={MailIcon} />
-            <PricingCard Icon={WhatsAppIcon} />
-            <PricingCard Icon={VerificationIcon} />
+            {services.map((service) => (
+              <PricingCard
+                key={service.type}
+                unit={service.unit}
+                receiving_price={service.receiving_price}
+                sending_price={service.sending_price}
+                subtext={service.subtext}
+                type={service.type}
+                Icon={service.icon}
+              />
+            ))}
           </SimpleGrid>
         </Box>
       )}
